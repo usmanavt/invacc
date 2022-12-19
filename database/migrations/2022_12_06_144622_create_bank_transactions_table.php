4c26d4142e9c216dc0e2ab0d9328e3b0c210219e
Karacraft <kcraftpk@gmail.com>
2022-12-19 09:16:06 +0500

    name corrected for table

--- database/migrations/2022_12_06_144622_create_bank_transactions_table.php
+++ database/migrations/2022_12_06_144622_create_bank_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatebankpaymentsTable extends Migration
+class CreateBankTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
