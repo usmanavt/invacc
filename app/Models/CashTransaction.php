ed2f29c3fd0f2a995aec232c99e38fbe6b0c9835
Karacraft <kcraftpk@gmail.com>
2022-12-25 19:18:18 +0500

    Cash transastion (receive/payment)

---
+++ app/Models/CashTransaction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CashTransaction extends Model
+{
+    use HasFactory;
+    public $filable=[
+        'bank_id',
+        'head_id',
+        'subhead_id',
+        'supplier_id',
+        'customer_id',
+        'transaction_type',
+        'conversion_rate',
+        'amount_fc',
+        'amount_pkr',
+        'cheque_date',
+        'cheque_no',
+        'descripton',
+        'status'
+    ];
+    /************** Relationships **************/
+    public function bank(){ return $this->belongsTo(Bank::class); }
+    public function head(){ return $this->belongsTo(Head::class); }
+    public function subhead(){ return $this->belongsTo(Subhead::class); }
+    public function supplier(){ return $this->belongsTo(Supplier::class); }
+    public function customer(){ return $this->belongsTo(Customer::class); }
+}
