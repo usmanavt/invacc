7523e6a52b2323df9ca143e0171d83c217d6c5dd
usmanavt <usman@auvitronics.com>
2023-05-24 20:13:24 +0500

    sales invoices updated

--- database/migrations/2023_05_06_092923_add_column_to_sales_invoices_details.php
+++ database/migrations/2023_05_06_092923_add_column_to_sales_invoices_details.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnToSaleInvoicesDetails extends Migration
+class AddColumnToSalesInvoicesDetails extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class AddColumnToSaleInvoicesDetails extends Migration
      */
     public function up()
     {
-        Schema::table('sale_invoices_details', function (Blueprint $table) {
+        Schema::table('sales_invoices_details', function (Blueprint $table) {
             $table->smallInteger('locid')->default(0);
             $table->smallInteger('salunitid')->default(0);
         });
