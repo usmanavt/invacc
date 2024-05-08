b04358893c668c3263d73e7f42f16c0389bd4370
Karacraft <42495441+Karacraft@users.noreply.github.com>
2024-05-08 20:13:11 +0500

    Update 2023_07_21_111125_create_quotations_table.php

--- database/migrations/2023_07_21_111125_create_quotations_table.php
+++ database/migrations/2023_07_21_111125_create_quotations_table.php
@@ -13,31 +13,31 @@ class CreateQuotationsTable extends Migration
      */
     public function up()
     {
-        Schema::create('quotations', function (Blueprint $table) {
-            $table->id();
-            $table->smallInteger('customer_id')->default(0);
-            $table->timestamp('saldate')->nullable();
-            $table->timestamp('valdate')->nullable();
-            $table->integer('qutno')->default(0);
-            $table->string('prno',50)->nullable();
-            $table->bigInteger('gpno')->default(0);
-            $table->decimal('discntper', 15, 3)->default(0.000);
-            $table->decimal('discntamt', 15, 3)->default(0.000);
-            $table->decimal('cartage', 15, 3)->default(0.000);
-            $table->decimal('rcvblamount', 15, 3)->default(0.000);
-            $table->smallInteger('saletaxper')->default(0);
-            $table->smallInteger('saletaxamt')->default(0);
-            $table->smallInteger('totrcvbamount')->default(0);
-            $table->string('Remarks', 150)->nullable();
-            $table->string('repcustname', 100)->nullable();
-            $table->string('repcustadrs', 150)->nullable();
-            $table->string('cashcustomer', 100)->nullable();
-            $table->string('cashcustadrs', 150)->nullable();
+        // Schema::create('quotations', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->smallInteger('customer_id')->default(0);
+        //     $table->timestamp('saldate')->nullable();
+        //     $table->timestamp('valdate')->nullable();
+        //     $table->integer('qutno')->default(0);
+        //     $table->string('prno',50)->nullable();
+        //     $table->bigInteger('gpno')->default(0);
+        //     $table->decimal('discntper', 15, 3)->default(0.000);
+        //     $table->decimal('discntamt', 15, 3)->default(0.000);
+        //     $table->decimal('cartage', 15, 3)->default(0.000);
+        //     $table->decimal('rcvblamount', 15, 3)->default(0.000);
+        //     $table->smallInteger('saletaxper')->default(0);
+        //     $table->smallInteger('saletaxamt')->default(0);
+        //     $table->smallInteger('totrcvbamount')->default(0);
+        //     $table->string('Remarks', 150)->nullable();
+        //     $table->string('repcustname', 100)->nullable();
+        //     $table->string('repcustadrs', 150)->nullable();
+        //     $table->string('cashcustomer', 100)->nullable();
+        //     $table->string('cashcustadrs', 150)->nullable();
 
 
-            $table->tinyInteger('status')->default(1); // 1=pending, 2 = completed
-            $table->timestamps();
-        });
+        //     $table->tinyInteger('status')->default(1); // 1=pending, 2 = completed
+        //     $table->timestamps();
+        // });
     }
 
     /**
@@ -47,6 +47,6 @@ class CreateQuotationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('quotation');
+        // Schema::dropIfExists('quotation');
     }
 }
