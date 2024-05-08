145b5652c390abaedb893f79f8f96bb47d6c2aa4
Karacraft <42495441+Karacraft@users.noreply.github.com>
2024-05-08 20:08:43 +0500

    Update 2023_05_01_124913_create_sale_invoices_table.php

--- database/migrations/2023_05_01_124913_create_sale_invoices_table.php
+++ database/migrations/2023_05_01_124913_create_sale_invoices_table.php
@@ -13,24 +13,24 @@ class CreateSaleInvoicesTable extends Migration
      */
     public function up()
     {
-        Schema::create('sale_invoices', function (Blueprint $table) {
-            $table->id();
-            $table->smallInteger('customer_id')->default(0);
-            $table->timestamp('saldate')->nullable();
-            $table->bigInteger('dcno')->default(0);
-            $table->bigInteger('billno')->default(0);
-            $table->bigInteger('gpno')->default(0);
-            $table->decimal('discntper', 15, 3)->default(0.000);
-            $table->decimal('discntamt', 15, 3)->default(0.000);
-            $table->decimal('cartage', 15, 3)->default(0.000);
-            $table->decimal('rcvblamount', 15, 3)->default(0.000);
-            $table->smallInteger('saletaxper')->default(0);
-            $table->smallInteger('saletaxamt')->default(0);
-            $table->smallInteger('totrcvbamount')->default(0);
-            $table->string('Remarks', 150)->nullable();
-            $table->timestamps();
+    //     Schema::create('sale_invoices', function (Blueprint $table) {
+    //         $table->id();
+    //         $table->smallInteger('customer_id')->default(0);
+    //         $table->timestamp('saldate')->nullable();
+    //         $table->bigInteger('dcno')->default(0);
+    //         $table->bigInteger('billno')->default(0);
+    //         $table->bigInteger('gpno')->default(0);
+    //         $table->decimal('discntper', 15, 3)->default(0.000);
+    //         $table->decimal('discntamt', 15, 3)->default(0.000);
+    //         $table->decimal('cartage', 15, 3)->default(0.000);
+    //         $table->decimal('rcvblamount', 15, 3)->default(0.000);
+    //         $table->smallInteger('saletaxper')->default(0);
+    //         $table->smallInteger('saletaxamt')->default(0);
+    //         $table->smallInteger('totrcvbamount')->default(0);
+    //         $table->string('Remarks', 150)->nullable();
+    //         $table->timestamps();
 
-    });
+    // });
     }
     /**
      * Reverse the migrations.
