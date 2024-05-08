aaad261872e9b46585e1abfe52c3d2352caea0a3
Karacraft <42495441+Karacraft@users.noreply.github.com>
2024-05-08 20:33:53 +0500

    Update and rename 2023_11_05_073413_add_column_to_sale_invoices_detail.php to 2023_11_05_073413_add_column_unitconver_to_sale_invoices_detail.php

--- database/migrations/2023_11_05_073413_add_column_unitconver_to_sale_invoices_detail.php
+++ database/migrations/2023_11_05_073413_add_column_unitconver_to_sale_invoices_detail.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddColumnToSaleInvoicesDetail extends Migration
+class AddColumnUnitconverToSaleInvoicesDetail extends Migration
 {
     /**
      * Run the migrations.
