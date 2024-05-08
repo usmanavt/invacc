f5af1d53307211bcce840da91c9bf49af1138820
Karacraft <42495441+Karacraft@users.noreply.github.com>
2024-05-08 20:25:39 +0500

    Update 2023_08_04_162231_b_add_column_to_sale_invoices_details.php

--- database/migrations/2023_08_04_162231_b_add_column_to_sale_invoices_details.php
+++ database/migrations/2023_08_04_162231_b_add_column_to_sale_invoices_details.php
@@ -13,7 +13,7 @@ class BAddColumnToSaleInvoicesDetails extends Migration
      */
     public function up()
     {
-        Schema::table('sale_invoices_details', function (Blueprint $table) {
+        Schema::table('sale_invoices_detail', function (Blueprint $table) {
             $table->string('brand', 50)->nullable();
             $table->Integer('cominvid')->default(0);
             $table->decimal('qtykgcrt', 12, 2)->default(0.00);
@@ -31,7 +31,7 @@ class BAddColumnToSaleInvoicesDetails extends Migration
      */
     public function down()
     {
-        Schema::table('sale_invoices_details', function (Blueprint $table) {
+        Schema::table('sale_invoices_detail', function (Blueprint $table) {
             //
         });
     }
