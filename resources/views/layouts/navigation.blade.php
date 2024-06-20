56d1f93f98e03fe3c11346ec67f16c212700af5b
usmanavt <usman@auvitronics.com>
2024-06-20 12:11:50 +0500

    update on 20-06-2024 2

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,20 +7,23 @@
 @extends('layouts.auth')
 
 
-{{-- @section('contents') --}}
+<div style="text-align:center;border-bottom: 1px solid blue ;font-weight: bold;font-size: 3rem;background: linear-gradient(red, blue)">
+    MUHAMMAD NAZEER & CO.
+    </div>
+
+    <div style="font-size:1rem;text-align:center;font-weight:bold">
+        Steam Pipes, Pipe Fitting, Flanges Valves, S.S Pipes PLOT NO. E-13, S.I.T.E AREA KARACHI MOBILE NO. 0333-3804744 Phone : 021-32588781, 021-32574285 ,
+        Fax : 021-32588782
+    </div>
+
+
+
+@section('contents')
 {{-- <img src="{{ asset('/images/logo.png') }}" alt="Logo" class="h-10"> --}}
 {{-- <link href="{{ asset('css/background.css') }}" rel="stylesheet"> --}}
 
 
-<div style="text-align:center;border-bottom: 1px solid blue ;font-weight: bold;font-size: 3rem;background: linear-gradient(red, blue)">
-{{-- <div style="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400"> --}}
-MUHAMMAD NAZEER & CO.
-</div>
 
-<div style="font-size:1rem;text-align:center;font-weight:bold">
-    Steam Pipes, Pipe Fitting, Flanges Valves, S.S Pipes PLOT NO. E-13, S.I.T.E AREA KARACHI MOBILE NO. 0333-3804744 Phone : 021-32588781, 021-32574285 ,
-    Fax : 021-32588782
-<div>
 
 
 {{-- <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"> --}}
@@ -32,9 +35,9 @@ MUHAMMAD NAZEER & CO.
 
 
 
-</div>
+{{-- </div> --}}
 
-{{-- @endsection --}}
+@endsection
 
 
 
